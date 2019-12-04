e5db43a71076b7fe21dca7227cc0bc2d2a377b96
jompero <dani.jompero@gmail.com>
2019-12-04 21:26:44 +0200

    NHLStatistics part 4

--- viikko6/QueryLanguage/src/main/java/statistics/Main.java
+++ viikko6/QueryLanguage/src/main/java/statistics/Main.java
@@ -10,15 +10,20 @@ public class Main {
         // "https://nhlstatisticsforohtu.herokuapp.com/players.txt"
 
         Statistics stats = new Statistics(new PlayerReaderImpl(url));
+        
+        QueryBuilder query = new QueryBuilder();
+        Matcher m = query.playsIn("NYR")
+            .hasAtLeast(5, "goals")
+            .hasFewerThan(10, "goals").build();
           
-        Matcher m = new And(
-            new HasAtLeast(20, "points"),
-            new Or( 
-                new PlaysIn("NYR"),
-                new PlaysIn("NYI"),
-                new PlaysIn("NJD")
-            )
-        );
+        // Matcher m = new And(
+        //     new HasAtLeast(20, "points"),
+        //     new Or( 
+        //         new PlaysIn("NYR"),
+        //         new PlaysIn("NYI"),
+        //         new PlaysIn("NJD")
+        //     )
+        // );
         
         for (Player player : stats.matches(m)) {
             System.out.println(player);
